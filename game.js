bef86f6e97f40578f6601dfc9c48636c4734b24a
Rakshit Jain <rakshitvipinjain21@gmail.com>
2023-01-02 20:00:27 +0530

    Initial-Commit

---
+++ game.js
@@ -0,0 +1,43 @@
+
+var buttonColors = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+    userClickedPattern.push(userChosenColour);
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+});
+
+var started = false;
+var level = 0;
+$(document).keypress(function(){
+    if(!started){
+        $("#level-title").text("Level "+level);
+        nextSequence();
+        started = true;
+    }
+});
+
+function nextSequence(){
+    ++level;
+    $("#level-title").text("Level "+level);
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100); 
+    playSound(randomChosenColour);
+}
+
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3");
+    audio.play();  
+}
+
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed");
+    },100)
+}
