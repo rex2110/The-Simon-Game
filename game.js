63501be592222a41164b347241bedc0c0104a5ac
Rakshit Jain <rakshitvipinjain21@gmail.com>
2023-01-02 21:34:25 +0530

    final-commit

--- game.js
+++ game.js
@@ -8,6 +8,7 @@ $(".btn").click(function(){
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
+    checkAnswer(userClickedPattern.lastIndexOf(userChosenColour));
 });
 
 var started = false;
@@ -21,6 +22,7 @@ $(document).keypress(function(){
 });
 
 function nextSequence(){
+    userClickedPattern=[];
     ++level;
     $("#level-title").text("Level "+level);
     var randomNumber = Math.floor(Math.random() * 4);
@@ -39,5 +41,30 @@ function animatePress(currentColour){
     $("#"+currentColour).addClass("pressed");
     setTimeout(function(){
         $("#"+currentColour).removeClass("pressed");
-    },100)
+    },100);
+}
+
+function checkAnswer(currentLevel){
+    if(gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+        if(userClickedPattern.length === gamePattern.length){
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        playSound("wrong");
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+            },200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+    }
+}
+
+function startOver(){
+    level = 0;
+    gamePattern=[];
+    started = false;
 }
